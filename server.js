9b4ab5c1d8b60986d3e320ef53ced6898497a083
kodapassey <kodapassey@charter.net>
2022-07-22 16:36:32 -0700

    added routes api routes for department

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
+const apiRoutes = require('./routes');
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
-    console.log(`server listening on port: ${PORT}`);
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
